b47db56fec3ef98a0771d73cbe71eac6bfc3813b
Lane Albrecht <ltalbrec@ucsc.edu>
2021-01-08 16:38:01 -0800

    Saving Progress

---
+++ pa1/Lex.c
@@ -0,0 +1,23 @@
+/*
+* Lane Albrecht, ltalbrec
+* 2021 Winter CSE 101 pa1
+* Lex.c
+* Take two command arguments which specify and input and output file
+*/
+
+#include <string.h> /* gives strcmp -> Lab Assign (3) */
+
+/* Quit if 2 command arguments are not present */
+// I think this uses scanf
+
+/* Count number of lines in Input File to create an array of same length */
+/* Populate array with each line in the Input File */
+
+/* Create a list that will lexicographic (alphabetic) sort the array */
+/* The list will have the indices of the array elements rather than the data */
+
+/* Tips: Use Insertion Sort as a guide, inserting 1 by 1 to do this */
+/* Don't directly use Insertion Sort */
+
+/*Print List to the Output File*/
+/* Order should be -> five four one three two */
